fe602d90159cbb73b5b6273664b58929541d8409
Brian Bycynski <high5bri@hotmail.com>
2018-02-12 11:05:38 -0500

    adding datepicker

--- src/components/AddExpensePage.js
+++ src/components/AddExpensePage.js
@@ -1,5 +1,10 @@
 import React from 'react';
+import ExpenseForm from './ExpenseForm';
 
-const AddExpensePage = () => <div>AddExpensePage</div>;
-
+const AddExpensePage = () => (
+    <div>
+        <h1>AddExpensePage</h1>
+        <ExpenseForm />
+    </div>
+);
 export default AddExpensePage;
